a2106f909d04963ddc1a413e288bbf3085017d8e
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2024-04-15 13:29:55 -0400

    Initial Commit

---
+++ Muse ME (but better)/Muse_ME__but_better_App.swift
@@ -0,0 +1,17 @@
+//
+//  Muse_ME__but_better_App.swift
+//  Muse ME (but better)
+//
+//  Created by Angelo Brown on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Muse_ME__but_better_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
