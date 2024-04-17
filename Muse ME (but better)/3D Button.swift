42bc2293ca5003816fcd5239b76047ef4295aeaf
Matthew Fails <matthew.c.fails.com>
2024-04-17 13:18:07 -0400

    Added 3D Button.

---
+++ Muse ME (but better)/3D Button.swift
@@ -0,0 +1,18 @@
+//
+//  3D Button.swift
+//  Muse ME (but better)
+//
+//  Created by Matthew Fails on 4/17/24.
+//
+
+import SwiftUI
+
+struct _D_Button: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _D_Button()
+}
